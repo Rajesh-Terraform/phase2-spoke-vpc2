1003cc8879b8af4f2524fede5c0cf7871de112d4
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-09-13 13:57:53 +0530

    phase2 spoke-vpc1 latest changes

--- versions.tf
+++ versions.tf
@@ -12,3 +12,25 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+
+terraform {
+  required_version = ">= 1.6.0"
+
+  backend "s3" {
+    bucket = "phase2-spoke-vpc2-terraform-state"
+    key    = "phase2-spoke-vpc2/terraform.tfstate"
+    region = "ap-south-1"  
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"  
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
